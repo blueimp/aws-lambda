c7a7e2e162c6de21281896388d3a68dfacbb9a09
Sebastian Tschan <mail@blueimp.net>
2015-09-27 22:39:41 +0200

    Use two spaces for indentation. Remove unnecessary log message. Remove template argument from generateRequestData method.

--- docker-hub-slack-notify/index.js
+++ docker-hub-slack-notify/index.js
@@ -13,64 +13,63 @@
 var slackWebhookPath = '/services/your/slack/hook';
 
 var template = '{push_data.pusher} pushed to ' +
-        '<{repository.repo_url}|{repository.repo_name}>.';
+  '<{repository.repo_url}|{repository.repo_name}>.';
 
 var requestOptions = {
-    hostname: 'hooks.slack.com',
-    port: 443,
-    path: slackWebhookPath,
-    method: 'POST'
+  hostname: 'hooks.slack.com',
+  port: 443,
+  path: slackWebhookPath,
+  method: 'POST'
 };
 
 function getNestedProperty(obj, property) {
-    property.replace(
-        // Matches native JavaScript notation in a String,
-        // e.g. '["doubleQuoteProp"].dotProp[2]'
-        /\[(?:'([^']+)'|"([^"]+)"|(\d+))\]|(?:(?:^|\.)([^\.\[]+))/g,
-        function (str, singleQuoteProp, doubleQuoteProp, arrayIndex, dotProp) {
-            var prop = dotProp || singleQuoteProp || doubleQuoteProp ||
-                (arrayIndex && parseInt(arrayIndex, 10));
-            if (str && obj) {
-                obj = obj[prop];
-            }
-        }
-    );
-    return obj;
+  property.replace(
+    // Matches native JavaScript notation in a String,
+    // e.g. '["doubleQuoteProp"].dotProp[2]'
+    /\[(?:'([^']+)'|"([^"]+)"|(\d+))\]|(?:(?:^|\.)([^\.\[]+))/g,
+    function (str, singleQuoteProp, doubleQuoteProp, arrayIndex, dotProp) {
+      var prop = dotProp || singleQuoteProp || doubleQuoteProp ||
+        (arrayIndex && parseInt(arrayIndex, 10));
+      if (str && obj) {
+        obj = obj[prop];
+      }
+    }
+  );
+  return obj;
 }
 
 function compileTemplate(template, data) {
-    return template.replace(
-        /\{(.+?)\}/g,
-        function (match, property) {
-            return getNestedProperty(data, property);
-        }
-    );
+  return template.replace(
+    /\{(.+?)\}/g,
+    function (match, property) {
+      return getNestedProperty(data, property);
+    }
+  );
 }
 
-function generateRequestData(template, event) {
-    return {
-        text: compileTemplate(template, event)
-    };
+function generateRequestData(event) {
+  return {
+    text: compileTemplate(template, event)
+  };
 }
 
 exports.handler = function(event, context) {
-    var req = require('https').request(requestOptions, function(res) {
-        var body = '';
-        console.log('Status:', res.statusCode);
-        console.log('Headers:', JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function(chunk) {
-            body += chunk;
-        });
-        res.on('end', function() {
-            console.log('Successfully processed HTTPS response');
-            if (res.headers['content-type'] === 'application/json') {
-                body = JSON.parse(body);
-            }
-            context.succeed(body);
-        });
+  var req = require('https').request(requestOptions, function(res) {
+    var body = '';
+    console.log('Status:', res.statusCode);
+    console.log('Headers:', JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function(chunk) {
+      body += chunk;
     });
-    req.on('error', context.fail);
-    req.write(JSON.stringify(generateRequestData(template, event)));
-    req.end();
+    res.on('end', function() {
+      if (res.headers['content-type'] === 'application/json') {
+        body = JSON.parse(body);
+      }
+      context.succeed(body);
+    });
+  });
+  req.on('error', context.fail);
+  req.write(JSON.stringify(generateRequestData(event)));
+  req.end();
 };
