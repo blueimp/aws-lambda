ac70d6851f0af3b398f6e5536fa1799b5d12a41a
Sebastian Tschan <mail@blueimp.net>
2017-01-12 18:35:35 +0100

    Add CloudWatch alarm notifications to Slack function.

---
+++ cloudwatch-alarm-to-slack/README.md
@@ -0,0 +1,41 @@
+# cloudwatch-alarm-to-slack
+[Amazon CloudWatch](https://aws.amazon.com/cloudwatch/) alarm notifications to
+[Slack](https://slack.com/) streaming function for
+[AWS Lambda](https://aws.amazon.com/lambda/).
+
+## Setup
+
+### Function configuration
+Add the function code to AWS Lambda with the following configuration options:  
+
+Key     | Value
+--------|--------------
+Runtime | Node.js 4.3
+Handler | index.handler
+Role    | AWSLambdaBasicExecutionRole
+Memory  | 128 (MB)
+Timeout | 3 sec
+KMS key | aws/lambda
+
+### Environment variables
+Set the following required environment variable for the Lambda function:
+
+Key     | Value
+--------|--------------
+webhook | [AWS KMS](https://aws.amazon.com/kms/) encrypted Slack WebHook URL.
+
+Set the following optional environment variable for the Lambda function:
+
+Key     | Value
+--------|--------------
+channel | Slack channel to send the notifications to.
+
+### Trigger configuration
+Add the desired [Amazon SNS](https://aws.amazon.com/sns/) topic as trigger for
+the Lambda function.
+
+## License
+Released under the [MIT license](https://opensource.org/licenses/MIT).
+
+## Author
+[Sebastian Tschan](https://blueimp.net/)
---
+++ cloudwatch-alarm-to-slack/index.js
@@ -0,0 +1,114 @@
+/*
+ * CloudWatch alarm notifications to Slack streaming function for AWS Lambda.
+ * https://github.com/blueimp/aws-lambda
+ *
+ * Required environment variables:
+ * - webhook: AWS KMS encrypted Slack WebHook URL.
+ *
+ * Optional environment variables:
+ * - channel: Slack channel to send the notifications to.
+ *
+ * Copyright 2017, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://opensource.org/licenses/MIT
+ */
+
+'use strict'
+
+const ENV = process.env
+if (!ENV.webhook) throw new Error('Missing environment variable: webhook')
+
+let webhook
+
+const AWS = require('aws-sdk')
+const url = require('url')
+const https = require('https')
+
+const statusColors = {
+  ALARM: 'danger',
+  INSUFFICIENT_DATA: 'warning',
+  OK: 'good'
+}
+
+function handleResponse (response, callback) {
+  const statusCode = response.statusCode
+  console.log('Status code:', statusCode)
+  let responseBody = ''
+  response
+    .on('data', chunk => { responseBody += chunk })
+    .on('end', chunk => {
+      console.log('Response:', responseBody)
+      if (statusCode >= 200 && statusCode < 300) {
+        callback(null, 'Request completed successfully.')
+      } else {
+        callback(new Error(`Request failed with status code ${statusCode}.`))
+      }
+    })
+}
+
+function post (requestURL, data, callback) {
+  const body = JSON.stringify(data)
+  const options = url.parse(requestURL)
+  options.method = 'POST'
+  options.headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(body)
+  }
+  console.log('Request options:', JSON.stringify(options))
+  console.log('Request body:', body)
+  https.request(options, response => { handleResponse(response, callback) })
+    .on('error', err => { callback(err) })
+    .end(body)
+}
+
+function buildSlackMessage (data) {
+  return {
+    channel: ENV.channel,
+    attachments: [{
+      fallback: data.AlarmName,
+      title: data.AlarmName,
+      text: data.AlarmDescription,
+      color: statusColors[data.NewStateValue],
+      fields: [
+        {
+          title: 'Status',
+          value: data.NewStateValue,
+          short: true
+        },
+        {
+          title: 'Region',
+          value: data.Region,
+          short: true
+        }
+      ]
+    }]
+  }
+}
+
+function processEvent (event, context, callback) {
+  console.log('Event:', JSON.stringify(event))
+  const snsMessage = event.Records[0].Sns.Message
+  console.log('SNS Message:', snsMessage)
+  const postData = buildSlackMessage(JSON.parse(snsMessage))
+  post(webhook, postData, callback)
+}
+
+function decryptAndProcess (event, context, callback) {
+  const kms = new AWS.KMS()
+  const enc = {CiphertextBlob: new Buffer(ENV.webhook, 'base64')}
+  kms.decrypt(enc, (err, data) => {
+    if (err) return callback(err)
+    webhook = data.Plaintext.toString('ascii')
+    processEvent(event, context, callback)
+  })
+}
+
+exports.handler = (event, context, callback) => {
+  if (webhook) {
+    processEvent(event, context, callback)
+  } else {
+    decryptAndProcess(event, context, callback)
+  }
+}
