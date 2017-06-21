a90731cb3ca5c7babce3ec8ea646f487b4689d5e
Sebastian Tschan <mail@blueimp.net>
2017-06-21 09:57:54 +0200

    Fix indentation (following eslint standard config).

--- cloudwatch-logs-to-elastic-cloud/index.js
+++ cloudwatch-logs-to-elastic-cloud/index.js
@@ -109,9 +109,9 @@ function buildSource (logEvent, payload, hasPipeline) {
 function transform (payload, hasPipeline) {
   let bulkRequestBody = ''
   const index = payload.logGroup
-                  .replace(/\W+|_+/g, '-')
-                  .replace(/^\-/, '')
-                  .toLowerCase()
+    .replace(/\W+|_+/g, '-')
+    .replace(/^-/, '')
+    .toLowerCase()
   payload.logEvents.forEach(logEvent => {
     bulkRequestBody += [
       JSON.stringify(buildAction(logEvent, payload, index)),
